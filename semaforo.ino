68b320023a76160d4b75e6ba2c76d30ae468401c
Jorge Jasso <jassojorgealberto@gmail.com>
2018-09-12 10:02:17 -0500

    Agregando el boton

--- semaforo.ino
+++ semaforo.ino
@@ -1,26 +1,30 @@
 #define verde 13
 #define amarillo 8
 #define rojo 7
+#define botonPeatonal 0
+
+boolean estadoBotonPeatonal = LOW; //Almacena el estado del botón peatonal, es decir si ha sido pulsado o no.
 
 void setup() {
   pinMode (verde, OUTPUT);
   pinMode (amarillo, OUTPUT);
   pinMode (rojo, OUTPUT);
+  pinMode (botonPeatonal, INPUT);
 }
 
 void loop() {
-    digitalWrite(verde, HIGH); //Enciende el verde
-    delay(10000); //Espera 10 segundos
-    digitalWrite(verde,LOW); //Apaga el verde
-    delay(10000); //Espera 10 segundos
-
-    digitalWrite(amarillo, HIGH); //Enciende el amarillo
-    delay(10000); //Espera 10 segundos
-    digitalWrite(amarillo,LOW); //Apaga el amarillo
-    delay(10000); //Espera 10 segundos
-
+  while(digitalRead(botonPeatonal)==LOW){ //Se repetira siempre y cuando el bóton peatonal este pulsado
+      estadoBotonPeatonal=LOW; //Indica que el botón peatonal ha sido precionado
+  }
+  if(estadoBotonPeatonal==LOW){ //Si el botón es igual a LOW significa que el botón ha sido pulsado
+    digitalWrite(verde, LOW); //Apaga el verde para posteriormente encender el amarillo
+    digitalWrite(amarillo, HIGH); //Se enciende el amarillo
+    delay(10000); //Espera 10 segundos 
+    digitalWrite(amarillo,LOW); //Apaga el amarillo  
     digitalWrite(rojo, HIGH); //Enciende el rojo
     delay(10000); //Espera 10 segundos
     digitalWrite(rojo,LOW); //Apaga el rojo
-    delay(10000); //Espera 10 segundos
+    digitalWrite(verde, HIGH); //Se vuelve a prender el verde que se mantendra asi hasta que otro peaton pida el cruce.
+    estadoBotonPeatonal=HIGH; //le asigna el valor de HIGH para decir que ya no esta en un estado de pulsado
+  }
 }
