e9279164de83b36f3ae5005c26f7f206d73951be
Jorge Jasso <jassojorgealberto@gmail.com>
2018-09-12 10:10:10 -0500

    Funcionalidades adicionales

--- semaforo.ino
+++ semaforo.ino
@@ -1,6 +1,7 @@
 #define verde 13
 #define amarillo 8
 #define rojo 7
+#define blanco 2
 #define botonPeatonal 0
 #define perilla A1
 
@@ -11,6 +12,7 @@ void setup() {
   pinMode (verde, OUTPUT);
   pinMode (amarillo, OUTPUT);
   pinMode (rojo, OUTPUT);
+  pinMode (blanco, OUTPUT); //Este pin servirá para mantener encedido un led siempre que el botón peatonal pueda ser pulsado y se apagara cuando aun no haya pasado el tiempo minimo del semáforo estando en verde.
   pinMode (botonPeatonal, INPUT);
   Serial.begin(9600); //Se establece la velocidad de la comunicación serial en 9600
 }
@@ -23,15 +25,27 @@ void loop() {
       estadoBotonPeatonal=LOW; //Indica que el botón peatonal ha sido precionado
   }
   if(estadoBotonPeatonal==LOW){ //Si el botón es igual a LOW significa que el botón ha sido pulsado
+    digitalWrite(blanco,LOW); //Apaga el led para indicar que no sé puede presionar el botón peatonal
     digitalWrite(verde, LOW); //Apaga el verde para posteriormente encender el amarillo
     digitalWrite(amarillo, HIGH); //Se enciende el amarillo
-    delay(10000); //Espera 10 segundos 
-    digitalWrite(amarillo,LOW); //Apaga el amarillo   
+    delay(6000); //Espera 6 segundos 
+    digitalWrite(amarillo,LOW); //Apaga el amarillo
+    /*
+      El ciclo permite que el amarillo prenda y apague a mayor velocidad, esto para indicar a los vehículos que el rojo ya se encenderá
+    */
+    for(int iteracion=0;iteracion<4;iteracion++){
+      delay(500);
+      digitalWrite(amarillo, HIGH); 
+      delay(500);
+      digitalWrite(amarillo,LOW);   
+    }
+      
     digitalWrite(rojo, HIGH); //Enciende el rojo
     delay(10000); //Espera 10 segundos
     digitalWrite(rojo,LOW); //Apaga el rojo
     digitalWrite(verde, HIGH); //Se vuelve a prender el verde que se mantendra asi hasta que otro peaton pida el cruce.
     delay(valorPerilla*10); //Hace una espera del valor de la perilla multiplicado por 10, es decir si el valor de la perilla es de 1000 la espera será de 10 segundos. Esto para darle un tiempo minimo al verde
+    digitalWrite(blanco, HIGH); //Una vez trasncurrido este tiempo el led blanco enciende para indicar que ya se puede precionar de nuevo el botón peatonal.
     estadoBotonPeatonal=HIGH; //le asigna el valor de HIGH para decir que ya no esta en un estado de pulsado
   }
 }
