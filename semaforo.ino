463718c4147bdb7b77bd7a7b24a6f6b143901c59
Jorge Jasso <jassojorgealberto@gmail.com>
2018-09-13 09:52:39 -0500

    Semaforo de los peatones

--- semaforo.ino
+++ semaforo.ino
@@ -4,6 +4,8 @@
 #define blanco 2
 #define botonPeatonal 0
 #define perilla A1
+#define verdePeaton 6
+#define rojoPeaton 5
 
 boolean estadoBotonPeatonal = LOW; //Almacena el estado del botón peatonal, es decir si ha sido pulsado o no.
 int valorPerilla=0; //Almacena el valor de la perilla
@@ -13,6 +15,8 @@ void setup() {
   pinMode (amarillo, OUTPUT);
   pinMode (rojo, OUTPUT);
   pinMode (blanco, OUTPUT); //Este pin servirá para mantener encedido un led siempre que el botón peatonal pueda ser pulsado y se apagara cuando aun no haya pasado el tiempo minimo del semáforo estando en verde.
+  pinMode (verdePeaton, OUTPUT);
+  pinMode (rojoPeaton, OUTPUT);
   pinMode (botonPeatonal, INPUT);
   Serial.begin(9600); //Se establece la velocidad de la comunicación serial en 9600
 }
@@ -39,10 +43,13 @@ void loop() {
       delay(500);
       digitalWrite(amarillo,LOW);   
     }
-      
+    digitalWrite(rojoPeaton, LOW);
     digitalWrite(rojo, HIGH); //Enciende el rojo
+    digitalWrite(verdePeaton, HIGH); //Le da el paso al peaton
     delay(10000); //Espera 10 segundos
     digitalWrite(rojo,LOW); //Apaga el rojo
+    digitalWrite(verdePeaton, LOW);
+    digitalWrite(rojoPeaton, HIGH); //Impide el paso del peaton
     digitalWrite(verde, HIGH); //Se vuelve a prender el verde que se mantendra asi hasta que otro peaton pida el cruce.
     delay(valorPerilla*10); //Hace una espera del valor de la perilla multiplicado por 10, es decir si el valor de la perilla es de 1000 la espera será de 10 segundos. Esto para darle un tiempo minimo al verde
     digitalWrite(blanco, HIGH); //Una vez trasncurrido este tiempo el led blanco enciende para indicar que ya se puede precionar de nuevo el botón peatonal.
